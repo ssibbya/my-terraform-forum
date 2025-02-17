4193109ec3db53b82df5b34261c7ec7c2f5e5acf
ssibbya <ssibbya@gmail.com>
2025-02-17 16:19:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,3 @@ provider "aws" {
     role_arn = var.aws_role_arn
   }
 }
-
-variable "aws_region" {}
-variable "aws_role_arn" {}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-cloud-test-bucket"
-}
