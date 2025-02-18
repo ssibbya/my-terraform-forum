3474d58f418ed817cdd4b6c2ed60d11a7e7108f3
ssibbya <ssibbya@gmail.com>
2025-02-18 19:46:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-
-  assume_role {
-    role_arn     = "arn:aws:iam::940482446197:role/TerraformCloudRole"
-    session_name = "TerraformCloudSession"
-  }
 }
 
 # VPC
