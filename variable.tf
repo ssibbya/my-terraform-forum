be415d385a5cc34179b53d3d6bee0780971fd3bf
ssibbya <ssibbya@gmail.com>
2025-02-18 19:46:06 +0530

    Update and rename variable.tf to variables.tf

--- variable.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "aws_role_arn" {
-  default = "arn:aws:iam::940482446197:role/TerraformCloudRole"
-}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "AWS region where resources will be deployed"
+  type        = string
+  default     = "us-east-1"
+}
