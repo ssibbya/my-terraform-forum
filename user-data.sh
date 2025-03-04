7b2f3db001dc58951a9f0436c675d88e8ca2dd91
ssibbya <ssibbya@gmail.com>
2025-03-04 19:26:41 +0530

    Delete user-data.sh

--- user-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Welcome to My Scalable Forum Application</h1>" | sudo tee /var/www/html/index.html
